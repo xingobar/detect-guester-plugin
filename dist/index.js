fd2c8262d297c0ff66f4f984af254625009fa6e5
xingobar <gary840227@gmail.com>
2018-08-25 20:42:05 +0700

    1.增加package.json

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,2 @@
 var detectGuester = require('./detect_guester.min');
-
-console.log(
-	detectGuester.detect.swipe({
-		target: '#test',
-		callback: function() {
-			console.log('teset');
-		},
-		direction: 'left'
-	})
-);
+module.exports = detectGuester.detect;
